24d318e2e5de3f2453a923b919edc9910671c054
liuwei3-s <liuwei3-s@360.cn>
2020-06-22 17:39:10 +0800

    add pthread_detach_thread test

--- thread_test/thread_test.cc
+++ thread_test/thread_test.cc
@@ -2,6 +2,7 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <atomic>
 
 void *thread_fun(void *arg)  {
   int i = 1;  
@@ -11,23 +12,61 @@ void *thread_fun(void *arg)  {
     pthread_testcancel();
   }
 
-  return (void *)0;  
+  return (void *)1;  
 }
 
-void test() {
-  void *ret = NULL;  
-  int iret=0;  
+// 测试 pthread_cancel
+void test_pthread_cancel() {
+  void* ret = NULL;  
   pthread_t tid;  
   pthread_create(&tid,NULL,thread_fun,NULL);  
   sleep(1);  
 
   pthread_cancel(tid);//取消线程  
   pthread_join(tid, &ret);  
-  std::cout << "thread exit code: " << ret << std::endl;
+  std::cout << "thread exit code: " << reinterpret_cast<long>(ret) << std::endl;
+}
+
+std::atomic<bool> kStop(false);
+void *thread_detach_fun(void *arg)  {
+  pthread_detach(pthread_self());
+
+  std::cout << "thread start" << std::endl;  
+
+#if 1 
+  while(1)  {
+#else
+  //while(kStop)  {
+#endif
+    sleep(1);
+#if 0
+    pthread_testcancel();
+#endif
+  }
+
+  std::cout << "thread end" << std::endl;  
+
+  return (void *)1;  
+}
+
+void test_detach_thread() {
+  pthread_t tid;  
+  pthread_create(&tid, NULL, thread_detach_fun, NULL);  
+  sleep(2);  
+
+  kStop = true;
+  //pthread_cancel(tid);//取消线程  
+  //void* ret = NULL;
+  //pthread_join(tid, &ret);  
+  std::cout << "main thread end" << std::endl;  
 }
 
 int main(int argc, char* argv[]) {
-  test();
+  //test_pthread_cancel();
+
+  test_detach_thread();
+
+  getchar();
 
   return 0;
 }

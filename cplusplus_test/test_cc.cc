585d9d9568bd561dc9244749780a4eb400b3ba01
lw <liuweixh@gmail.com>
2020-06-26 11:45:50 +0800

    add test case: move constructor and move operator =

--- cplusplus_test/test_cc.cc
+++ cplusplus_test/test_cc.cc
@@ -23,9 +23,33 @@ class MyString {
       Clone(my_string.data_);
     }
 
+    MyString(MyString&& my_string) noexcept {
+      std::cout << "Call to move constructor" << std::endl;
+      data_ = my_string.data_;
+      length_ = my_string.length_;
+
+      my_string.data_ = nullptr;
+      my_string.length_ = 0;
+    }
+
+    ~MyString() {
+      std::cout << "Call to destructor" << std::endl;
+    }
+
     MyString& operator =(const MyString& my_string) {
       std::cout << "Call to operator =" << std::endl;
       Clone(my_string.data_);
+      return *this;
+    }
+
+    MyString& operator =(MyString&& my_string) {
+      std::cout << "Call to move operator =" << std::endl;
+      data_ = my_string.data_;
+      length_ = my_string.length_;
+
+      my_string.data_ = nullptr;
+      my_string.length_ = 0;
+
       return *this;
     }
 
@@ -42,27 +66,67 @@ class MyString {
       if (nullptr != data) {
         length_ = strlen(data) + 1;
         data_ = new char[length_];
-        memcpy(data_, data, strlen(data));
+        std::memcpy(data_, data, strlen(data));
         data_[length_] = '\0';
       }
     }
 
+    void Move(MyString&& my_string) {
+      data_ = my_string.data_;
+      length_ = my_string.length_;
+
+      my_string.data_ = nullptr;
+      my_string.length_ = 0;
+    }
   private:
     char* data_;
     size_t length_;
 };
+
+MyString MakeMyString(const char* data) {
+  MyString my_str(data);
+  return my_str;
+}
 
 void testRightValue() {
   int i = 0;
   int& lr = i;
   const int& lr2 = 10;
 
+#if 0
   MyString str1(kStrData);
   MyString str2 = str1;
 
   MyString str3;
   str3 = str1;
+#endif
+
+  /*
+   * 1. 如果没有禁用返回值优化，不管是否定义了move constructor, 都输出如下：
+   * Call to char* constructor
+   * MyString => abcd
+   * Call to destructor
+   * 
+   * 2. 如果禁用返回值优化-fno-elide-constructors，输出如下：
+   * Call to char* constructor
+   * Call to move constructor MakeMyString中的局部变量move constructor到此函数的返回值临时变是中
+   * Call to destructor
+   * Call to move constructor
+   * Call to destructor
+   * MyString => abcd
+   * Call to destructor
+   *
+   * 3. 如果没定义move constructor, 则上面调用的move constructor换成copy constructor
+
+  MyString str4(MakeMyString(kStrData));
+  str4.Show();
+  */
+
+  MyString str5;
+  str5 = MakeMyString(kStrData);
+  str5.Show();
 }
+
 } //namespace 
 
 int main(int argc, char* argv[]) {
